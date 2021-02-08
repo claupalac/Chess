966e8cc80e864de68aebffb45d2792890a78f081
Claudio <claudio@MacBook-Pro-de-Claudio.local>
2021-02-08 12:41:48 -0400

    Implement of new control movement classes

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChessTable chessTable = new ChessTable(8,8);
+            King king = new King();
+            chessTable.PutChessman(king,1,1);
+            Player player1 = new Player();
+            player1.ShowMovesFromChessman(king,chessTable);
+        }
+    }
+}
